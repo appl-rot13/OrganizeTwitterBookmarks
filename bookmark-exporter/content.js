a5ae871bcf9089eefd54416a3b915695e61497c9
rot <appl.rot13@gmail.com>
2026-05-02 17:59:29 +0900

    Add auto-scrolling and CSV downloads

--- bookmark-exporter/content.js
+++ bookmark-exporter/content.js
@@ -1,4 +1,55 @@
 (() => {
+  const autoScroll = ({ speed = 10, ignoreCount = 120 } = {}) => {
+    return new Promise(resolve => {
+      let lastScrollY = window.scrollY;
+      let idleCount = 0;
+
+      function step() {
+        window.scrollBy(0, speed);
+
+        const currentScrollY = window.scrollY;
+        if (currentScrollY > lastScrollY) {
+          lastScrollY = currentScrollY;
+          idleCount = 0;
+        } else {
+          idleCount++;
+        }
+
+        if (idleCount < ignoreCount) {
+          window.requestAnimationFrame(step);
+        } else {
+          resolve();
+        }
+      }
+
+      window.requestAnimationFrame(step);
+    });
+  }
+
+  const downloadAsCsv = (data, filename) => {
+    if (!Array.isArray(data) || data.length === 0) {
+      return;
+    }
+
+    const header = Object.keys(data[0]);
+    const csv = [
+      header,
+      ...data.map(obj => header.map(key => obj[key]))
+    ].map(row => row.join(',')).join('\n');
+
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(url);
+  };
+
+  let scrollPromise = null;
+
   const hrefs = new Set();
   const observer = new MutationObserver(() => {
     document.querySelectorAll('article[data-testid="tweet"]').forEach(tweet => {
@@ -13,7 +64,23 @@
       }
 
       hrefs.add(href);
-      console.log(hrefs.size + ': ' + href);
+      console.log(href);
+
+      if (!scrollPromise) {
+        scrollPromise = autoScroll();
+        scrollPromise.then(() => {
+          observer.disconnect();
+
+          const data = Array.from(hrefs)
+            .reverse()
+            .map((value, index) => ({
+              No: index + 1,
+              URL: value,
+            }));
+
+          downloadAsCsv(data, 'bookmarks.csv');
+        });
+      }
     });
   });
 
